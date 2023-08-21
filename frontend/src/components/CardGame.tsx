30de076a71e7b2abf1bce617dfc1aee1fca796d5
AleNS <isalejanaranjo@gmail.com>
2023-08-21 14:45:20 +0200

    #2 frontend initial setting completed

---
+++ frontend/src/components/CardGame.tsx
@@ -0,0 +1,14 @@
+//animacion al darle click
+
+import Card from 'react-bootstrap/Card';
+
+export const CardGame = () => {
+  return (
+
+
+       <Card data-testid="card">
+
+      </Card> 
+   
+  )
+}
---
+++ frontend/src/pages/Game.tsx
@@ -0,0 +1,12 @@
+//opcional timer
+//grid level
+//logic, compair if there is a match and identify when there is only a pair missing to finish it
+import React from 'react'
+import { CardGame } from '../components/CardGame'
+
+export const Game = () => {
+  return (
+   
+    <CardGame/>
+  )
+}

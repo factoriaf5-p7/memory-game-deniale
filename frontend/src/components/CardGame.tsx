fe1f2ca73419ac57d0dc3fc1ab507938d611806b
AleNS <isalejanaranjo@gmail.com>
2023-08-22 11:24:55 +0200

    Implementing Shuffle method to disorder the images array

--- frontend/src/components/CardGame.tsx
+++ frontend/src/components/CardGame.tsx
@@ -1,9 +1,16 @@
 //animacion al darle click
 
 import Card from 'react-bootstrap/Card';
+import data from '../data/data.json'
+import { shuffle } from '@vitest/utils';
+
+
 
 export const CardGame = () => {
   
+  const duplicatedCards = [...data, ...data]
+  shuffle(duplicatedCards)
+  console.log(duplicatedCards)
   return (
 
 

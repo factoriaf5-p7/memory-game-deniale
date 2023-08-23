b69c78dbd2a5b9a43ac2b36247c70f4708508cd5
AleNS <isalejanaranjo@gmail.com>
2023-08-23 16:13:31 +0200

    updated navbar test, which is now navbarComponent

--- frontend/src/components/Navbar.tsx
+++
@@ -1,9 +0,0 @@
-//restart, puntuacion sesion actual, modal avatar(la informacion del perfil (puntuacion general, partidas jugadas, nombre)), modal settings
-import { Container } from 'react-bootstrap'
-
-export const Navbar = () => {
-  return (
-    <Container data-testid="navbar">Navbar</Container>
-  )
-}
-
--- frontend/src/tests/components/navbarComponent.test.tsx
+++ frontend/src/tests/components/navbarComponent.test.tsx
@@ -1,11 +1,11 @@
 import { describe, expect,test } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import {Navbar} from '../../components/NavbarComponent';
+import {NavbarComponent} from '../../components/NavbarComponent';
 
 
 describe('Navbar', () => {
     test('renders a Navbar', () => {
-      render(<Navbar/>);
+      render(<NavbarComponent/>);
        
       const modalElement = screen.getByTestId('navbar');    
       expect(modalElement).toBeInTheDocument();

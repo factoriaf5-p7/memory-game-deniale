4819cab838e7e88a916affac20b9a96421da079d
denizozerdogan <131254999+denizozerdogan@users.noreply.github.com>
2023-08-23 16:01:29 +0200

    #4 branch with test theme

--- frontend/src/components/ModalSettings.tsx
+++ frontend/src/components/ModalSettings.tsx
@@ -1,13 +1,20 @@
 //dificultad, cambiar el tema(Call buttons/theme)
-import { Container } from "react-bootstrap"
-import Modal from "react-bootstrap/Modal"
+import { Container } from "react-bootstrap";
+import Modal from "react-bootstrap/Modal";
+import { ThemeSelector } from "./ThemeSelector";
+import { ModalProps } from "react-bootstrap/Modal"; 
 
-
-export const ModalSettings = () => {
+interface ModalSettingsProps extends ModalProps {
+  onChangeTheme: (theme: string) => void;
+}
+export const ModalSettings = ({ onChangeTheme, show, onHide }: ModalSettingsProps) => {
   return (
     <Container data-testid="modal-settings">
-        <Modal >ModalSettingsr</Modal>
+      <Modal show={show} onHide={onHide}>
+        <Modal.Body>
+          <ThemeSelector onChangeTheme={onChangeTheme} />
+        </Modal.Body>
+      </Modal>{" "}
     </Container>
-    
-  )
-}
+  );
+};
---
+++ frontend/src/components/NavbarComponent.tsx
@@ -0,0 +1,41 @@
+//restart, puntuacion sesion actual, modal avatar(la informacion del perfil (puntuacion general, partidas jugadas, nombre)), modal settings
+
+import  Container  from "react-bootstrap/Container";
+import { ModalSettings } from "./ModalSettings";
+import { useCardGameLogic } from "../hooks/useCardGameLogic";
+import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button"; 
+import { useState } from "react";
+
+export const NavbarComponent = () => {
+  const { resetGame } = useCardGameLogic();
+  const [showSettingsModal, setShowSettingsModal] = useState(false);
+  const [selectedTheme, setSelectedTheme] = useState('superhero'); 
+
+  const openSettingsModal = () => {
+    setShowSettingsModal(true);
+  };
+
+  const closeSettingsModal = () => {
+    setShowSettingsModal(false);
+  };
+
+  const handleThemeChange = (theme: string) => {
+    setSelectedTheme(theme);
+    resetGame(theme); // Pass the selected theme to reset the game
+    closeSettingsModal(); // Close the modal after selecting a theme
+  };
+
+  return (
+    <Navbar>
+      <Container data-testid="navbar">
+        <Button onClick={openSettingsModal}>Open Settings</Button>
+        <ModalSettings
+          onChangeTheme={handleThemeChange}
+          show={showSettingsModal}
+          onHide={closeSettingsModal}
+        />
+      </Container>{" "}
+    </Navbar>
+  );
+};

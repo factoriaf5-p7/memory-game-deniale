fd289be11509bfe8bbcfa59bf02078dcb1d4913b
denizozerdogan <131254999+denizozerdogan@users.noreply.github.com>
2023-08-25 17:16:25 +0200

    #7 images added, testing for navbar and modal ending updated

--- frontend/src/components/UserForm.tsx
+++ frontend/src/components/UserForm.tsx
@@ -6,81 +6,137 @@ import Button from "react-bootstrap/Button";
 import Alert from "react-bootstrap/Alert";
 
 export interface UserData {
-  name: string;
-}
-
-export const UserForm = () => {
-  const [name, setName] = useState("");
-  const [isNameSaved, setIsNameSaved] = useState(false);
-  const [showAlert, setShowAlert] = useState(false); // Changed from showToast
-  const [alertMessage, setAlertMessage] = useState(""); // To store the error message
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-
-    try {
-      const response: AxiosResponse = await axios.post(
-        "http://localhost:3000/user",
-        { name }
-      );
-
-      if (response.status === 201) {
-        const userData = response.data;
-        localStorage.setItem("userName", userData.name);
+    name: string;
+  }
+  export const UserForm = () => {
+    const [name, setName] = useState("");
+    const [isNameSaved, setIsNameSaved] = useState(false);
+    const [showAlert, setShowAlert] = useState(false);
+    const [alertMessage, setAlertMessage] = useState("");
+    const [selectedImage, setSelectedImage] = useState("");
+  
+    useEffect(() => {
+      const storedName = localStorage.getItem("userName");
+      if (storedName) {
+        setName(storedName);
         setIsNameSaved(true);
-        setName("");
       }
-    } catch (error: any) {
-      setAlertMessage(
-        "This name already exists. Please choose a different name."
+    }, []);
+  
+    const handleSubmit = async (e: React.FormEvent) => {
+      e.preventDefault();
+  
+      try {
+        const response: AxiosResponse = await axios.post(
+          "http://localhost:3000/user",
+          { name }
+        );
+  
+        if (response.status === 201) {
+          const userData = response.data;
+          localStorage.setItem("userName", userData.name);
+          setIsNameSaved(true);
+          setName("");
+        }
+      } catch (error: any) {
+        setAlertMessage(
+          "This name already exists. Please choose a different name."
+        );
+        setShowAlert(true);
+      }
+    };
+  
+    const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const file = e.target.files?.[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = (event) => {
+          setSelectedImage(event.target?.result as string);
+        };
+        reader.readAsDataURL(file);
+      }
+    };
+  
+    useEffect(() => {
+      if (selectedImage) {
+        localStorage.setItem("userImage", selectedImage);
+      }
+    }, [selectedImage]);
+  
+    useEffect(() => {
+      if (showAlert) {
+        const timer = setTimeout(() => {
+          setShowAlert(false);
+        }, 3000);
+  
+        return () => {
+          clearTimeout(timer);
+        };
+      }
+    }, [showAlert]);
+  
+    return (
+        <Container>
+          <div className="user-form-box p-3" style={{ backgroundColor: "#f5f5f5", borderRadius: "8px" }}>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group className="mb-3">
+                <Form.Label htmlFor="name" className="styled-label">
+                  Name:
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  id="name"
+                  name="name"
+                  value={name}
+                  onChange={(e) => {
+                    setName(e.target.value);
+                    setShowAlert(false);
+                  }}
+                  required
+                  disabled={isNameSaved}
+                  className="styled-input"
+                />
+              </Form.Group>
+    
+              <Form.Group className="mb-3">
+                <Form.Label>Upload Image:</Form.Label>
+                <Form.Control
+                  type="file"
+                  accept="image/*"
+                  onChange={handleImageUpload}
+                  className="styled-input"
+                />
+              </Form.Group>
+    
+              {selectedImage && (
+                <div className="image-container mb-3">
+                  <img
+                    src={selectedImage}
+                    alt="Uploaded"
+                    className="img-fluid rounded-circle"
+                    style={{ width: "6rem", height: "6rem" }}
+                  />
+                </div>
+              )}
+    
+              <div className="d-flex justify-content-center mt-3">
+                <Button variant="primary" type="submit" disabled={isNameSaved} className="styled-button">
+                  Submit
+                </Button>
+              </div>
+    
+              {showAlert && (
+                <Alert
+                  variant="danger"
+                  onClose={() => setShowAlert(false)}
+                  dismissible
+                  className="mt-3"
+                >
+                  {alertMessage}
+                </Alert>
+              )}
+            </Form>
+          </div>
+        </Container>
       );
-      setShowAlert(true);
-    }
-  };
-  useEffect(() => {
-    if (showAlert) {
-      const timer = setTimeout(() => {
-        setShowAlert(false);
-      }, 3000);
-
-      return () => {
-        clearTimeout(timer);
-      };
-    }
-  }, [showAlert]);
-  return (
-    <Container>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-3">
-          <Form.Label htmlFor="name">Name:</Form.Label>
-          <Form.Control
-            type="text"
-            id="name"
-            name="name"
-            value={name}
-            onChange={(e) => {
-              setName(e.target.value);
-              setShowAlert(false);
-            }}
-            required
-            disabled={isNameSaved}
-          />
-        </Form.Group>
-
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-
-        {showAlert && (
-          <Alert
-            variant="danger"
-            onClose={() => setShowAlert(false)}
-            dismissible
-          >
-            {alertMessage}
-          </Alert>
-        )}
-      </Form>
-    </Container>
-  );
-};
+    };
--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,18 +1,34 @@
 //despues de la animación pedir name y avatar, button start
 import {PrimaryButton} from "../components/buttons/PrimaryButton"
-import { Container} from "react-bootstrap"
-import Form from 'react-bootstrap/Form';
+import { Container, Row, Col} from "react-bootstrap"
 import { UserForm } from "../components/UserForm";
 import { Link } from "react-router-dom";
 
 export const Landing= () => {
   return (
     <Container>
-     <Form action="" data-testid="form"></Form>
-      <UserForm />
-      <Link to="/game"> 
-        <PrimaryButton>Go to Game</PrimaryButton>
-      </Link>
+      <Row style={{ margin: "10rem auto" }}>
+        <Col
+          xs={12}
+          md={6}
+          className="mb-5 mb-md-0 d-flex flex-column align-items-center justify-content-center"
+        >
+          <div data-testid="form">
+            <UserForm />
+          </div>
+        </Col>
+        <Col
+          xs={12}
+          md={6}
+          className="d-flex flex-column align-items-center justify-content-center"
+        >
+          <Link to="/game">
+            <PrimaryButton>Go to Game</PrimaryButton>
+          </Link>
+        </Col>
+      </Row>
     </Container>
-  )
-}
+  );
+};
+
+
